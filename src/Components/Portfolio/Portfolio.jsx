ffc899203d8ef52dce0fc5f08b774f201426ba81
Abhishek Kumar <abhishek.kr.11511@gmail.com>
2023-10-26 10:46:33 +0530

    Added Links of projects in portfolio

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -13,31 +13,40 @@ const projects = [
     title : "Zaffran",
     subtitle : 'The fine dining exprience like never before',
     img: zaffran,
-    description : "Indulge in a culinary voyage at our Indian-Persian fine dining establishment, where the rich, aromatic allure of saffron harmonizes with exquisite flavors for a truly enchanting gastronomic experience."
+    description : "Indulge in a culinary voyage at our Indian-Persian fine dining establishment, where the rich, aromatic allure of saffron harmonizes with exquisite flavors for a truly enchanting gastronomic experience.",
+    link : 'https://github.com/AbhishekKr11511/Zaffran-website'
   },
   {
     title : "GPT-3",
     subtitle : 'A Place to get the latest info about AI',
     img: gpt,
-    description : "Explore the cutting-edge world of AI and machine learning on our website. Stay informed with the latest updates, insightful blogs, and innovative features, unlocking the potential of tomorrow's technology today."
+    description : "Explore the cutting-edge world of AI and machine learning on our website. Stay informed with the latest updates, insightful blogs, and innovative features, unlocking the potential of tomorrow's technology today.",
+    link : 'https://github.com/AbhishekKr11511/Project-GPT-3-website-React'
+
   },
   {
     title : "3D Shirt Design",
     subtitle : 'Customise your T-shirt, add custom logos.',
     img: threejs,
-    description : "Unleash your creativity with our 3D T-shirt design application. Craft personalized, one-of-a-kind shirts with custom colors and images, bringing your unique style to life effortlessly and vividly."
+    description : "Unleash your creativity with our 3D T-shirt design application. Craft personalized, one-of-a-kind shirts with custom colors and images, bringing your unique style to life effortlessly and vividly.",
+    link : 'https://github.com/AbhishekKr11511/Project-3D-Customize-Your-Shirt'
+
   },
   {
     title : "Poogle",
     subtitle : 'A Search engine, equiped with search results, images, and videos',
     img: poogle,
-    description : "Discover simplicity with our minimalist search engine. Effortlessly find search results, images, and videos that match your query, all while enjoying a clean, distraction-free user experience."
+    description : "Discover simplicity with our minimalist search engine. Effortlessly find search results, images, and videos that match your query, all while enjoying a clean, distraction-free user experience.",
+    link : 'https://github.com/AbhishekKr11511/Project-Poogle-Search-Engine'
+
   },
   {
     title : "Cyber Assets",
     subtitle : 'Find your latest crypto Stats and news here',
     img: cryptolayout,
-    description : "Empower your crypto journey with our comprehensive website. Get real-time updates on the dynamic market, dive into breaking news, explore in-depth crypto exchange data, and discover the top cyber currencies of today. Elevate your knowledge and make informed decisions within the vast realm of digital assets, all on one platform"
+    description : "Empower your crypto journey with our comprehensive website. Get real-time updates on the dynamic market, dive into breaking news, explore in-depth crypto exchange data, and discover the top cyber currencies of today. Elevate your knowledge and make informed decisions within the vast realm of digital assets, all on one platform",
+    link : 'https://github.com/AbhishekKr11511/Project-Cryptocurrency-Price-Tracker-Exchanges-News'
+
   },
 ]
 
@@ -78,7 +87,7 @@ const Single = ({project}) => {
           <span>{project.title}</span>
           <p className='subtitle'>{project.subtitle}</p>
           <p className='description'>{project.description}</p>
-          <button> GitHub &lt;/&gt;</button>
+          <button onClick={()=>window.location.href = project.link}> GitHub &lt;/&gt;</button>
         </motion.div>
         
       </div>
