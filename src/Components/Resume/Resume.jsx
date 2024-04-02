61a0cf2e150cb157786fa3acf6f3d764b6739844
Abhishek Kumar <128050928+AbhishekKr11511@users.noreply.github.com>
2024-04-02 14:22:35 +0530

    Fix : Updated the Better Resume Download

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -3,7 +3,7 @@ import Arrow from '../../Assets/Arrow.png'
 import leftbrain from '../../Assets/left brain.png'
 import rightbrain from '../../Assets/right brain.png'
 import {motion} from "framer-motion";
-import resume from '../../Assets/Download/Abhisheks Resume.pdf'
+import resume from '../../Assets/Download/Abhisheks Final Resume.pdf'
 
 const cardVariants = {
   offscreen: {
